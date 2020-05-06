d142c1c5db62e3def89c2214b360919b3b89f318
Joe Handzik <jhandzik@nvidia.com>
2020-05-06 08:40:09 -0700

    Leverage path.join to handle a path argument without a trailing /
    Signed-Off-By: Joe Handzik <jhandzik@nvidia.com>

--- imagine.py
+++ imagine.py
@@ -1,3 +1,4 @@
+import os
 import numpy
 import click
 from PIL import Image
@@ -20,22 +21,23 @@ def main():
 @click.option('--seed', default=0)
 def create(path, name, width, height, count, seed):
     click.echo("Creating {} JPEG files located at {} of {}x{} resolution with a base filename of {}".format(count, path, width, height, name))
-   
-    start_time = perf_counter()
+
+    combined_path = os.path.join(path, name)
 
     #Expected to yield a thread pool equivalent to the number of CPU cores in the system
     pool = Pool()
-    result = pool.starmap(imageCreation, ((path, name, width, height, seed, n) for n in range(count)))
+    start_time = perf_counter()
+    result = pool.starmap(imageCreation, ((combined_path, width, height, seed, n) for n in range(count)))
 
     stop_time = perf_counter()
     
     click.echo("Created {} files in {} seconds".format(count, stop_time-start_time))
 
-def imageCreation(path, name, width, height, seed, n):
+def imageCreation(combined_path, width, height, seed, n):
     numpy.random.seed(seed + n)
     a = numpy.random.rand(height,width,3) * 255
     im_out = Image.fromarray(a.astype('uint8')).convert('RGB')
-    im_out.save('%s%s%d.jpg' % (path, name, n))
+    im_out.save('%s%d.jpg' % (combined_path, n))
     return
 
 if __name__ == "__main__":
